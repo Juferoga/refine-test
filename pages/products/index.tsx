ebbee01125e6f5e33f99a77d50185d7c9364e6f1
Juferoga <juancholoco2000.61@gmail.com>
2024-02-01 23:31:38 -0500

    asy filter, select

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,5 +1,6 @@
-import { useTable } from "@refinedev/antd";
+import { FilterDropdown, useTable } from "@refinedev/antd";
 import { List, Table } from "antd";
+import { Radio } from "antd/lib";
 
 
 const ProductList = () => {
@@ -9,7 +10,20 @@ const ProductList = () => {
   return <List>
     <Table {...tableProps} rowKey="id">
       <Table.Column dataIndex="id" title="ID" />
-      <Table.Column dataIndex="name" title="Nombre" />
+      <Table.Column dataIndex="name" title="Nombre"  
+        filterDropdown = {
+          (props) => {
+              return (
+                <FilterDropdown {...props}>
+                  <Radio.Group>
+                    <Radio value="Bose QuietComfort 35 II Wireless Headphones">Bose QuietComfort 35 II Wireless Headphones</Radio>
+                    <Radio value="Felipe">Felipe</Radio>
+                    <Radio value="Jhon">Jhon</Radio>
+                  </Radio.Group>
+                </FilterDropdown>
+              )
+          }
+        } />
       <Table.Column dataIndex="description" title="Descripción" />
       <Table.Column dataIndex="price" title="Precio" />
     </Table>
